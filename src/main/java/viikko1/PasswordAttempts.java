a91713b0ad7f5b39b74b8b490181d93cd622a60e
Saattaj4 <markku.putaala@icloud.com>
2025-09-05 11:10:20 +0300

    Valmis viikkotehtävä 1

--- src/main/java/viikko1/PasswordAttempts.java
+++ src/main/java/viikko1/PasswordAttempts.java
@@ -2,7 +2,11 @@ package viikko1;
 
 public class PasswordAttempts {
     public static String login(String[] tries) {
-        // TODO
-        return null;
+        for (int i = 0; i < 3 && i < tries.length; i++){
+            if (tries[i].equals("java123")){
+                return "Tervetuloa!";
+            }
+        }
+        return "Liian monta virheellistä yritystä.";
     }
 }
--- src/main/java/viikko1/SortThree.java
+++ src/main/java/viikko1/SortThree.java
@@ -2,7 +2,25 @@ package viikko1;
 
 public class SortThree {
     public static String ascending(int a, int b, int c) {
-        // TODO
+        if (a<=b && b<=c){
+            return a + "," + b + "," +c;
+        }
+            else if (b <= c && c <= a){
+                return b + "," + c + "," +a;
+            }
+            else if (b <= a && a <= c){
+                return b + ","+ a + "," + c;
+            }
+            else if (c <= b && b <= a){
+                return c + "," + b + "," + a;
+            }
+            else if (c <= a && a <= b){
+                return c + "," + a + "," + b;
+            }
+            else if (a <= c && c <= b){
+                return a + "," + c + "," + b;
+            }
+        
         return null;
+        }
     }
-}
